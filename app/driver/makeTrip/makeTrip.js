dfede638addbc2bd82ebe64545f5ec0d6bce659e
clcyau <clcyau@mansci-db.uwaterloo.ca>
2018-02-28 15:37:03 -0500

    Drivers can create a trip, and select seats that are available

---
+++ app/driver/makeTrip/makeTrip.js
@@ -0,0 +1,145 @@
+'use strict';
+
+angular.module('dropOff.makeTrip', ['ngRoute', 'firebase'])
+
+.controller('makeTripCtrl', ['$scope', 'CommonProp', '$location', '$firebaseArray', 'seatCalc', '$firebaseObject',
+ function($scope, CommonProp, $location, $firebaseArray, seatCalc, $firebaseObject){
+    $scope.username = CommonProp.getUser();
+    $scope.userType = CommonProp.getPermission();
+    $scope.success = false;
+
+    if(!$scope.username && $scope.userType != "driver"){
+        $location.path('/login');
+    }
+
+    var locationRef = firebase.database().ref().child('locations');
+    $scope.locations = $firebaseArray(locationRef);
+
+    var timeRef = firebase.database().ref().child('times');
+    $scope.times = $firebaseArray(timeRef);
+
+    $scope.trip = {};
+    var tripRef = firebase.database().ref().child('trips');
+    $scope.trips = $firebaseArray(tripRef);
+
+    $scope.createTrip = function(){
+      $scope.trip.seats = seatCalc.getTripSeats($scope.seats);
+      $scope.trip.driver = CommonProp.getUID();
+
+      $scope.trips.$add(
+        $scope.trip
+      ).then(function(ref){
+        showSuccess();
+      }, function(error){
+        console.log(error);
+      });
+    }
+
+    function showSuccess(){
+      $scope.success = true;
+			window.setTimeout(function(){
+				$scope.$apply(function(){
+          $scope.success = false;
+				});
+			}, 2000);
+    }
+
+    // *********** SKETCHY WAY OF ADDING DATA FOR NOW
+    // var locations = ["Waterloo", "London", "Kingston"];
+
+    // for(var i = 0; i < locations.length; i++){
+    //     $scope.locations.$add({
+    //         name: locations[i]
+    //     }
+    //     ).then(function(ref){
+    //         console.log(ref);
+    //     }, function(error){
+    //         console.log(error);
+    //     });
+    // }
+
+    // var times = ["Early Morning", "Mid-morning", "Afternoon", "Evening", "Late Night"];
+    // for(var i = 0; i < times.length; i++){
+    //     $scope.times.$add({
+    //         description: times[i]
+    //     });
+    // }
+    // ***********************END SKETCHY
+    
+    $scope.obj = [
+      [{
+        col: 1,
+        row: 1,
+        isAvailable: false,
+        isSeat: false,
+        takenBy: false
+      }, {
+        col: 2,
+        row: 1,
+        isAvailable: false,
+        isSeat: true,
+        takenBy: false
+      }],
+      [{
+        col: 1,
+        row: 2,
+        isAvailable: false,
+        isSeat: true,
+        takenBy: false
+      }, {
+        col: 2,
+        row: 2,
+        isAvailable: false,
+        isSeat: true,
+        takenBy: false
+      }, {
+        col: 3,
+        row: 2,
+        isAvailable: false,
+        isSeat: true,
+        takenBy: false
+      }]
+    ];
+    $scope.isDisabled = false;
+    $scope.seats = $scope.obj;
+    // $scope.rowLetter = seatCalc.rowStack($scope.obj);
+    // $scope.seatCount = 0;
+    
+    $scope.clickSeat = function(seat) {
+      if (seat.isSeat && !$scope.isDisabled) {
+        if (seat.isAvailable) {
+          seat.isAvailable = false;
+          // $scope.seatCount--;
+        } else {
+          seat.isAvailable = true;
+          // $scope.seatCount++;
+        }
+      }
+    }
+}])
+  
+.service('seatCalc', function() {
+    var rowLetter = [];
+
+    this.rowStack = function(obj) {
+      for (var i = 0, j = 65; i < obj.length; i++, j++) {
+      rowLetter.push(String.fromCharCode(j));
+      }
+      return rowLetter;
+    }
+
+    this.getTripSeats = function(seats) {
+      var tripSeats = [];
+
+      for (var row = 0; row < seats.length; row++){
+        for (var col = 0; col < seats[row].length; col++){
+          var currentSeat = seats[row][col];
+          if (currentSeat.isSeat && currentSeat.isAvailable){
+            tripSeats.push(currentSeat);
+          }
+        }
+      }
+
+      return tripSeats;
+    }
+});
