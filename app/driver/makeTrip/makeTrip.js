073969b2a0a213e1a9efa285fab5644839a7db20
clcyau <clcyau@mansci-db.uwaterloo.ca>
2018-03-12 11:27:57 -0400

    including compiled bootstrap

--- app/driver/makeTrip/makeTrip.js
+++ app/driver/makeTrip/makeTrip.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('dropOff.makeTrip', ['ngRoute', 'firebase'])
+angular.module('dropOff.makeTrip', ['ngRoute', 'firebase', 'ngSanitize', 'ui.select'])
 
 .controller('makeTripCtrl', ['$scope', 'CommonProp', '$location', '$firebaseArray', 'seatCalc', '$firebaseObject',
  function($scope, CommonProp, $location, $firebaseArray, seatCalc, $firebaseObject){
@@ -23,6 +23,14 @@ angular.module('dropOff.makeTrip', ['ngRoute', 'firebase'])
 
     var locationRef = firebase.database().ref().child('locations');
     $scope.locations = $firebaseArray(locationRef);
+
+    $scope.tagTransform = function(newTag){
+      console.log(newTag);
+      var item = {
+        name: newTag
+      };
+      return item;
+    }
 
     $scope.trip = {};
     var tripRef = firebase.database().ref().child('trips');
