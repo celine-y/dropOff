3f4c24c013cfdd3d0fa8becfb6ab4d8fa887d2dc
kkitt <taya.k@hotmail.com>
2018-03-14 16:41:12 -0400

    User seat booking

--- app/rider/bookRides.js
+++ app/rider/bookRides.js
@@ -10,14 +10,91 @@ angular.module('dropOff.bookRides', ['ngRoute', 'firebase','ui.bootstrap'])
 
 		var tripId = $routeParams.tripId;
 		var driverId = '';
+
 		//Trip Info
 		var fbRefTrip = firebase.database().ref().child('trips').child(tripId);
 		var trip = $firebaseObject(fbRefTrip);
-		trip.$bindTo($scope, "trip");
+		$scope.trip = trip;
+		console.log($scope.trip);
 
-		$scope.getDriver = function(){
+		//User Trips
+		$scope.userTrip = {};
+		var fbRefUserTrip = '';
+
+		$scope.trip.$loaded().then(function(){
+			//Driver Info
 			driverId = $scope.trip.driver;
-		}
+			var fbRefDriver = firebase.database().ref().child('drivers').child(driverId);
+			var driver = $firebaseObject(fbRefDriver);
+			$scope.driver = driver;
+			console.log($scope.driver);
+
+			//Seat Info
+			//alter $scope.seat based on seat info
+			angular.forEach($scope.trip.seats, function(obj, idx){
+				var row = obj.row-1;
+				var col = obj.col-1;
+				var status = obj.isAvailable;
+
+				$scope.seats[row][col]["isSeat"] = status;
+			});
+
+			$scope.getColor = function(){
+				return "#"+$scope.driver.color;
+			};
+
+			$scope.clickSeat = function(seat){
+				if (seat.isSeat){
+					if (seat.isAvailable){
+						seat.isAvailable = false;
+					}else{
+						seat.isAvailable = true;
+					}					
+				}
+			};
+
+			$scope.clickConfirm = function (){
+				var user
+			};
+
+
+		});
+
+		$scope.seats = [
+		[{
+			col: 1,
+			row: 1,
+			isAvailable: false,
+			isSeat: false,
+			takenBy: false
+		}, {
+			col: 2,
+			row: 1,
+			isAvailable: false,
+			isSeat: false,
+			takenBy: false
+		}],
+		[{
+			col: 1,
+			row: 2,
+			isAvailable: false,
+			isSeat: false,
+			takenBy: false
+		}, {
+			col: 2,
+			row: 2,
+			isAvailable: false,
+			isSeat: false,
+			takenBy: false
+		}, {
+			col: 3,
+			row: 2,
+			isAvailable: false,
+			isSeat: false,
+			takenBy: false
+		}]
+		];
+
 
 	}
-]);
+	]);
